28db94ffdaaf7f269a0fbdcdd8059ea9759f68fa
JoabAssefa <144885834+JoabAssefa@users.noreply.github.com>
2023-12-10 15:11:05 +0300

    Update ex1-forloop.py with the print
    Update ex1-forloop.py with the print for the first time form GitHub account

--- ex1-forloop.py
+++ ex1-forloop.py
@@ -1,3 +1,4 @@
 print( "first modification for 2nd commit")
 print (" the first modification and saving using vs code")
 print ("understanding editor as defult commit ")
+print( "added this line from the github website for the first time")
